454d69d33102d6c76f10915a84733990925866c6
Raj Verma <info.mrverma@gmail.vom>
2020-07-06 16:22:06 +0530

    3rd day

---
+++ array.js
@@ -0,0 +1,23 @@
+var names = ["Rohan","Jmaes","Superman","Rock"];
+
+console.log(names[0]);
+
+var cars = new Array("BMW","AUDI","HUNDAI");
+
+console.log(cars[0]);
+
+cars[1]="SKODA";
+
+console.log(cars)
+
+console.log(cars.length)
+
+console.log(cars.sort());
+
+console.log(names[names.length - 1]);
+
+names.forEach(name => {
+    console.log(name);
+});
+
+
---
+++ number.js
@@ -0,0 +1,22 @@
+var num = 456123;
+console.log(typeof num);
+
+var num2 = num.toString();
+console.log(typeof num2)
+
+console.log(num.toExponential(6))
+
+var num3 = 150.2568;
+console.log(num3.toFixed(0))
+console.log(num3.toFixed(1))
+console.log(num3.toFixed(2))
+
+var num4 = '120';
+var num5 = 120;
+
+console.log(parseInt(num4) + num5 );
+
+console.log(parseInt("10 23"))
+
+console.log(Number.MAX_VALUE)
+
